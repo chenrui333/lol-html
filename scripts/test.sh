0a5c88925a3cfbc6e43d87013fae73d798266505
Kornel <kornel@cloudflare.com>
2024-11-08 15:10:43 +0000

    CDPATH compatibility

--- scripts/test.sh
+++ scripts/test.sh
@@ -10,8 +10,8 @@ echo "=== Running C API tests... ==="
 prove -e 'cargo' run ::  --manifest-path=./c-api/c-tests/Cargo.toml
 
 echo "=== Building fuzzing test case code to ensure that it uses current API... ==="
-(cd fuzz/test_case && cargo check)
+cargo check --manifest-path=./fuzz/test_case/Cargo.toml
 
 echo "=== Building the tooling test case code to ensure it uses the current API... ==="
-(cd tools/parser_trace/ && cargo check)
-(cd tools/selectors_ast/ && cargo check)
+cargo check --manifest-path=./tools/parser_trace/Cargo.toml
+cargo check --manifest-path=./tools/selectors_ast/Cargo.toml
